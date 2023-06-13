e411c1d88484be07f9cead89ba4a20054b594b2e
donreegan <136298907+donreegan@users.noreply.github.com>
2023-06-13 15:01:17 +0530

    task files

---
+++ object.js
@@ -0,0 +1,5 @@
+        
+            let person=[{name:"Vijay"},{gender:"Male"},{age:40},{fname:"anbu"},
+            {mname:"jothi"},{color:"blue"},7,4,3,];
+            console.log(person);
+            document.getElementById("ex1").innerHTML= "name is " +typeof "anbu";     
